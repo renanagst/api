ae0a4834824c1f8b25c9277f50b47d0236eea74a
Renan Santos <renanagst@gmail.com>
2021-03-14 20:33:49 -0300

    Add files via upload

---
+++ portal.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+# result = requests.get ('https://google.com')
+# print (result.text)
+
+# result = requests.get ('http://192.168.50.45:3000/')
+# res = json.loads(result.text)
+# print (res['name'])
+
+# import requests as r
+# import json
+
+# response = r.get("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+# result = json.loads(response.text)
+# print("There are", len(result), "states in Brazil")
+
+# for state in result:
+#     print(state['id'], ' | ', state['nome'])
